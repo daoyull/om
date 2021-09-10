da34abb214a6509817213b5a585177561b383a24
daoyu <daoyu5313@163.com>
2021-09-10 16:47:59 +0800

    完成根据用户ID获取菜单功能

--- server/src/main/java/com/zxz/server/mapper/MenuMapper.java
+++ server/src/main/java/com/zxz/server/mapper/MenuMapper.java
@@ -3,14 +3,16 @@ package com.zxz.server.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zxz.server.pojo.Menu;
 
+import java.util.List;
+
+
 /**
- * <p>
- *  Mapper 接口
- * </p>
  *
  * @author zxz
  * @since 2021-09-09
  */
 public interface MenuMapper extends BaseMapper<Menu> {
 
+
+    List<Menu> getMenuByAdminId(Integer id);
 }
--- server/src/main/java/com/zxz/server/service/impl/MenuServiceImpl.java
+++ server/src/main/java/com/zxz/server/service/impl/MenuServiceImpl.java
@@ -2,9 +2,14 @@ package com.zxz.server.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zxz.server.mapper.MenuMapper;
+import com.zxz.server.pojo.Admin;
 import com.zxz.server.pojo.Menu;
 import com.zxz.server.service.IMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +21,17 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements IMenuService {
+
+
+    @Autowired
+    private MenuMapper menuMapper;
+
+
+
+    @Override
+    public List<Menu> getMenuByAdminId() {
+        return menuMapper.getMenuByAdminId(((Admin) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
+    }
+
 
 }
