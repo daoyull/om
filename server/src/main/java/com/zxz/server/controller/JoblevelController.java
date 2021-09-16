5bad1f01cba5a44d105031a54f34bc69415bd3df
Administrator <daoyu5313@163.com>
2021-09-16 19:39:04 +0800

    更新职称管理

--- server/src/main/java/com/zxz/server/controller/JoblevelController.java
+++ server/src/main/java/com/zxz/server/controller/JoblevelController.java
@@ -1,8 +1,16 @@
 package com.zxz.server.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.zxz.server.pojo.Joblevel;
+import com.zxz.server.pojo.RespBean;
+import com.zxz.server.service.IJoblevelService;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * <p>
@@ -13,7 +21,53 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-09-09
  */
 @RestController
-@RequestMapping("/joblevel")
+@RequestMapping("/system/basic/joblevel")
 public class JoblevelController {
+
+    @Autowired
+    private IJoblevelService joblevelService;
+
+    @ApiModelProperty("获取所有职称")
+    @GetMapping("/")
+    public List<Joblevel> getAllJobLevel(){
+        return joblevelService.list();
+    }
+
+    @ApiModelProperty("增加职称")
+    @PostMapping("/")
+    public RespBean addJobLevel(@RequestBody Joblevel joblevel){
+        joblevel.setCreateDate(LocalDateTime.now());
+        if(joblevelService.save(joblevel)){
+            return RespBean.success("添加成功");
+        }
+        return RespBean.error("添加失败");
+    }
+
+    @ApiModelProperty("更新职称信息")
+    @PutMapping("/")
+    public RespBean updateJobLevel(@RequestBody Joblevel joblevel){
+        if(joblevelService.updateById(joblevel)){
+            return RespBean.success("更新成功");
+        }
+        return RespBean.error("更新失败");
+    }
+
+    @ApiModelProperty("删除职称")
+    @DeleteMapping("/{id}")
+    public RespBean deleteJobLevel(@PathVariable Integer id){
+        if(joblevelService.removeById(id)){
+            return RespBean.success("删除成功");
+        }
+        return RespBean.error("删除失败");
+    }
+
+    @ApiModelProperty("批量删除职称")
+    @DeleteMapping("/")
+    public RespBean deleteJobLevelIds(Integer[] ids){
+        if(joblevelService.removeByIds(Arrays.asList(ids))){
+            return RespBean.success("删除成功");
+        }
+        return RespBean.error("删除失败");
+    }
 
 }
