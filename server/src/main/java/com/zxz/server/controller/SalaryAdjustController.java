3ccb6df6d2d5904845a89def3101e9acbe7e0f9c
Administrator <daoyu5313@163.com>
2021-10-11 12:54:37 +0800

    完成员工奖惩

--- server/src/main/java/com/zxz/server/controller/SalaryAdjustController.java
+++ server/src/main/java/com/zxz/server/controller/SalaryAdjustController.java
@@ -1,8 +1,17 @@
 package com.zxz.server.controller;
 
 
+import com.zxz.server.pojo.RespBean;
+import com.zxz.server.pojo.SalaryAdjust;
+import com.zxz.server.service.ISalaryAdjustService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 /**
  * <p>
@@ -15,5 +24,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/salary-adjust")
 public class SalaryAdjustController {
+
+    @Autowired
+    private ISalaryAdjustService salaryAdjustService;
+
+    @ApiOperation("员工奖金调整")
+    @PostMapping("/add")
+    public Object updataSalary(@RequestBody SalaryAdjust salaryAdjust){
+        salaryAdjust.setAsDate(LocalDate.now());
+        if(salaryAdjustService.save(salaryAdjust)){
+            return RespBean.success("成功");
+        }
+        return RespBean.error("失败");
+    }
 
 }
