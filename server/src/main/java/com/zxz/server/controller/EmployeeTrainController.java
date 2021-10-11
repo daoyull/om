f453a229562f4c6f4e3e2f9d1afbeed966d2ece4
Administrator <daoyu5313@163.com>
2021-10-11 13:31:30 +0800

    完成员工培训

--- server/src/main/java/com/zxz/server/controller/EmployeeTrainController.java
+++ server/src/main/java/com/zxz/server/controller/EmployeeTrainController.java
@@ -1,8 +1,18 @@
 package com.zxz.server.controller;
 
 
+import com.zxz.server.pojo.Employee;
+import com.zxz.server.pojo.EmployeeTrain;
+import com.zxz.server.pojo.RespBean;
+import com.zxz.server.service.IEmployeeTrainService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 /**
  * <p>
@@ -15,5 +25,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/employee-train")
 public class EmployeeTrainController {
+
+    @Autowired
+    private IEmployeeTrainService employeeTrainService;
+
+    @ApiOperation("员工培训")
+    @PostMapping("/add")
+    public Object employeeTrain(@RequestBody EmployeeTrain employeeTrain){
+        employeeTrain.setTrainDate(LocalDate.now());
+        if(employeeTrainService.save(employeeTrain)){
+            return RespBean.success("成功");
+        }
+        return RespBean.error("失败");
+    }
 
 }
